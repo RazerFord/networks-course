faa7157a9880b8cde41c920daf4afae0c791402e
RazerFord <desk10567@gmail.com>
2024-03-04 12:36:00 +0300

    added while loop

--- lab03/singlethreadedwebserver/src/main.go
+++ lab03/singlethreadedwebserver/src/main.go
@@ -40,80 +40,83 @@ func main() {
 
 	logger.Printf("[ INFO ] server created: http://%v:%v \n", addr.IP, addr.Port)
 
-	tcp, err := server.AcceptTCP()
+	for {
 
-	if err != nil {
-		panic("connection error")
+		tcp, err := server.AcceptTCP()
+
+		if err != nil {
+			panic("connection error")
+		}
+
+		logger.Printf("[ INFO ] connection accepted\n")
+
+		reader := bufio.NewReader(tcp)
+		req, err := http.ReadRequest(reader)
+		req.Body.Close()
+
+		if err != nil {
+			logger.Printf("[ ERROR ] %v\n", err)
+		} else {
+			logger.Printf("[ INFO ] message read\n")
+		}
+
+		params, err := req.URL.Parse(req.RequestURI)
+
+		if err != nil {
+			logger.Printf("[ ERROR ] %v\n", err)
+		}
+
+		values := params.Query()
+		file := values.Get(reqParam)
+
+		if !values.Has(reqParam) {
+			logger.Printf("[ ERROR ] %v parameter not found\n", reqParam)
+			return
+		} else {
+			logger.Printf("[ INFO ] %v = %v\n", reqParam, file)
+		}
+
+		fbytes, err := os.ReadFile(file)
+
+		resp := http.Response{
+			Proto:      req.Proto,
+			Request:    req,
+			ProtoMajor: req.ProtoMajor,
+			ProtoMinor: req.ProtoMinor,
+			Header:     make(http.Header),
+		}
+
+		if os.IsNotExist(err) {
+			logger.Printf("[ ERROR ] file not found\n")
+
+			resp.Status = http.StatusText(http.StatusNotFound)
+			resp.StatusCode = http.StatusNotFound
+		} else if err != nil {
+			logger.Printf("[ ERROR ] file opening error\n")
+
+			resp.Status = http.StatusText(http.StatusNotFound)
+			resp.StatusCode = http.StatusNotFound
+		} else {
+			resp.Body = io.NopCloser(bytes.NewBuffer(fbytes))
+			resp.ContentLength = int64(len(fbytes))
+			resp.Status = http.StatusText(http.StatusAccepted)
+			resp.StatusCode = http.StatusAccepted
+			resp.Body.Close()
+		}
+
+		var byteBuff bytes.Buffer
+		w := bufio.NewWriter(&byteBuff)
+
+		err = resp.Write(w)
+
+		if err != nil {
+			logger.Printf("[ ERROR ] error writing HTTP response\n")
+			return
+		}
+
+		w.Flush()
+		tcp.Write(byteBuff.Bytes())
+
+		logger.Printf("[ INFO ] response sent successfully: %v\n", resp.ContentLength)
 	}
-
-	logger.Printf("[ INFO ] connection accepted\n")
-
-	reader := bufio.NewReader(tcp)
-	req, err := http.ReadRequest(reader)
-	req.Body.Close()
-
-	if err != nil {
-		logger.Printf("[ ERROR ] %v\n", err)
-	} else {
-		logger.Printf("[ INFO ] message read\n")
-	}
-
-	params, err := req.URL.Parse(req.RequestURI)
-
-	if err != nil {
-		logger.Printf("[ ERROR ] %v\n", err)
-	}
-
-	values := params.Query()
-	file := values.Get(reqParam)
-
-	if !values.Has(reqParam) {
-		logger.Printf("[ ERROR ] %v parameter not found\n", reqParam)
-		return
-	} else {
-		logger.Printf("[ INFO ] %v = %v\n", reqParam, file)
-	}
-
-	fbytes, err := os.ReadFile(file)
-
-	resp := http.Response{
-		Proto:      req.Proto,
-		Request:    req,
-		ProtoMajor: req.ProtoMajor,
-		ProtoMinor: req.ProtoMinor,
-		Header:     make(http.Header),
-	}
-
-	if os.IsNotExist(err) {
-		logger.Printf("[ ERROR ] file not found\n")
-
-		resp.Status = http.StatusText(http.StatusNotFound)
-		resp.StatusCode = http.StatusNotFound
-	} else if err != nil {
-		logger.Printf("[ ERROR ] file opening error\n")
-
-		resp.Status = http.StatusText(http.StatusNotFound)
-		resp.StatusCode = http.StatusNotFound
-	} else {
-		resp.Body = io.NopCloser(bytes.NewBuffer(fbytes))
-		resp.ContentLength = int64(len(fbytes))
-		resp.Status = http.StatusText(http.StatusAccepted)
-		resp.StatusCode = http.StatusAccepted
-	}
-	resp.Body.Close()
-
-	var byteBuff bytes.Buffer
-	w := bufio.NewWriter(&byteBuff)
-
-	err = resp.Write(w)
-
-	if err != nil {
-		logger.Printf("[ ERROR ] error writing HTTP response\n")
-		return
-	}
-
-	w.Flush()
-	tcp.Write(byteBuff.Bytes())
-
-	logger.Printf("[ INFO ] response sent successfully: %v\n", resp.ContentLength)
 }
