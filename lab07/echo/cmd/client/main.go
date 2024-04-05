2ac8f81bfec2126638d9fc9fb96da3bf02f3fea7
RazerFord <desk10567@gmail.com>
2024-04-06 00:24:55 +0300

    solved 3

--- lab07/echo/cmd/client/main.go
+++ lab07/echo/cmd/client/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"math"
 	"math/rand"
 	"net"
 	"os"
@@ -62,25 +63,45 @@ func (c *Client) Write(msg string) (int, error) {
 
 func main() {
 	port := flag.Int("port", 8080, "client port")
-	length := flag.Int("len", 10, "message length")
+	length := flag.Int("len", 63, "message length")
 	n := flag.Int("n", 10, "number of messages")
 	flag.Parse()
 
 	client := NewClient(*port)
 
-	for i := range *n {
+	min := math.MaxFloat64
+	max := -1.0
+	avg := 0.0
+	sum := 0.0
+	c := 0
+	s := time.Now()
+	for range *n {
 		start := time.Now()
 		client.Write(genString(*length))
 		msg, err := client.ReadTimeout(time.Second)
 		end := time.Now()
 
 		if errors.Is(err, os.ErrDeadlineExceeded) {
-			logging.Warn("Request timed out %d", i + 1)
+			fmt.Printf("request timed out\n")
 		} else {
-			logging.Info("received message: %s", msg)
-			logging.Info("Ping %d %v", i+1, end.Sub(start).Seconds())
+			l := len([]byte(msg))
+			addr := client.conn.RemoteAddr().String()
+			t := float64(end.Sub(start)) / 1e6
+
+			sum += t
+			c++
+			min = math.Min(min, t)
+			max = math.Max(max, t)
+			avg = sum / float64(c)
+
+			fmt.Printf("%d bytes from %v: time=%.2f ms\n", l, addr, t)
 		}
 	}
+	e := time.Now()
+	m := float64(*n)
+	fmt.Printf("\n--- %s ping statistics ---\n", client.conn.RemoteAddr().String())
+	fmt.Printf("%d packets transmitted, %d received, %d%% packet loss, time %0.fms\n", *n, c, int((m-float64(c))/m*100), float64(e.Sub(s))/1e6)
+	fmt.Printf("rtt min/avg/max = %.2f/%.2f/%.2f ms\n", min, avg, max)
 }
 
 func genString(n int) string {
