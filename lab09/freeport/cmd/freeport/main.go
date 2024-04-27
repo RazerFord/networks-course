db0dda00a7e4073b6e8c75f74be0c1d9f922a1f1
RazerFord <desk10567@gmail.com>
2024-04-27 14:35:36 +0300

    solved two task

---
+++ lab09/freeport/cmd/freeport/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+)
+
+const maxPort = 1 << 16 // :)
+
+func main() {
+	ip := flag.String("ip", "0.0.0.0", "ip address")
+	start := flag.Int("start", 0, "start of port ranges")
+	end := flag.Int("end", maxPort-1, "end of port range")
+	flag.Parse()
+
+	ports := []int{}
+	for port := range *end - *start + 1 {
+		port += *start
+
+		addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", *ip, port))
+		if err != nil {
+			continue
+		}
+
+		conn, err := net.ListenTCP("tcp", addr)
+		if err != nil {
+			continue
+		}
+		conn.Close()
+
+		ports = append(ports, port)
+	}
+	fmt.Printf("ip: %s\n", *ip)
+	fmt.Printf("free ports: %v\n", ports)
+}
